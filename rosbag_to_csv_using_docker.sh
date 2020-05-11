61610ff701d216194e202c9cf07031f78f3541ea
Hayato Mizushima <hayato.mizushima@tier4.jp>
2020-05-11 20:20:16 +0900

    feat: switch image

--- rosbag_to_csv_using_docker.sh
+++ rosbag_to_csv_using_docker.sh
@@ -1,10 +1,9 @@
-DOCKER_IMAGE="autoware/autoware:1.12.0-kinetic-cuda" # rosが動けば本来なんでもいい
+DOCKER_IMAGE="ros:melodic-ros-base-bionic"
 FILTER_TOPIC="/current_pose"
 INPUT_ROSBAG="2020-05-11-19-59-53.bag"
 CSV_FILE_NAME="out.csv"
 
 docker run --rm -v ${PWD}:/rosbag -i ${DOCKER_IMAGE} bash << EOF
-source ~/Autoware/install/setup.bash
 rostopic echo -b "/rosbag/${INPUT_ROSBAG}" -p ${FILTER_TOPIC} > "/rosbag/${CSV_FILE_NAME}"
 EOF
 
